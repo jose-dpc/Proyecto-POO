37b504887614c473ae21ef29943efd1686b3786d
Jorge Ibarra <alejandro.ibarra.maya@gmail.com>
2025-05-07 08:11:38 -0600

    Aqui subo la base de datos de direcciones, se supone que en estos momentos se deberia agregar correctamente

---
+++ BaseDeDatos/src/App.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class App {
+    @SuppressWarnings("CallToPrintStackTrace")
+    public static Connection conectar() {
+        Connection conexion=null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String url = "jdbc:sqlite:direcciones.db";
+            conexion = DriverManager.getConnection(url);
+            System.out.println("Conexión exitosa");
+        } catch (ClassNotFoundException e) {
+            System.out.println("No se encontró el driver SQLite ");
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la base de datos SQLite");
+            e.printStackTrace();
+        }
+        return conexion;
+    }
+    
+        public static void main(String[] args) {
+            conectar();}
+
+    
+}
